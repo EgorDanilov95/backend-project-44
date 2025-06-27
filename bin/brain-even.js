d159ce30aed0d6712c85f64bb1140f1ba6739ca1
Egor Danilov <danilov-egor-e@yandex.ru>
2025-06-27 06:45:08 +0000

    fixed some problems with path

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import evenGame from '/home/Egor/Desktop/backend-project-44/crs/games/even.js'
-import game from '/home/Egor/Desktop/backend-project-44/crs/index.js'
+import evenGame from '../crs/games/even.js'
+import game from '../crs/index.js'
 game(evenGame)
