aab577adc7283a1477fd0921aab9bf8aa27ba787
Egor Danilov <danilov-egor-e@yandex.ru>
2025-07-03 12:31:28 +0000

    fixed

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,8 +3,7 @@ export const isPrime = (num) => {
   if (num < 2) {
     return false
   }
-
-  for (let i = 2; i <= Math.sqrt(num); i += 2) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
       return false
     }
